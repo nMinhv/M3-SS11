6e0bfa415e1601e6720e9f5499745b35bc46f693
nMinhv <aquarius.mizu0902@gmail.com>
2023-10-04 08:03:13 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import sun.misc.Queue;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.add(3);
+    }
+}
---
+++ src/ra/bt/bt1/Main.java
@@ -0,0 +1,15 @@
+package ra.bt.bt1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Integer>queue = new LinkedList<>();
+        for (int i = 0; i <10 ; i++) {
+            queue.offer((int) (Math.random()*10));
+        }
+        System.out.println(queue);
+    }
+}
